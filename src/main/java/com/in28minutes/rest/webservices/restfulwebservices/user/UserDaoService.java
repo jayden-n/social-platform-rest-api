81f1d2d7b961468fd64d1dcf4a159ec24921f96a
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-09-19 21:22:08 -0400

    feat: implemented GET methods for User Resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -7,13 +7,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Component
 public class UserDaoService {
     // The Data Access Object is basically an object or an interface that provides access to an underlying database or any other persistence storage.
 
     // JPA/Hibernate to talk to Database
 
-    private static List<User> users = new ArrayList<>();
+    private static final List<User> users = new ArrayList<>();
 
     static {
         users.add(new User(1,"Jayden", LocalDate.now().minusYears(30)));
@@ -23,5 +24,26 @@ public class UserDaoService {
 
     public List<User> findAll() {
         return users;
+    }
+    public User findById(int id) {
+
+        return users.stream()
+               .filter(user -> user.getId() == id) // Filter users with matching ID
+                .findFirst() // Get the first matching user, or null if none found
+                .orElse(null); // Return null if no matching user was found
+
+
+//        SOLUTION 1:
+//        Predicate<? super User> predicate = user -> user.getId().equals(id);
+//       return users.stream().filter(predicate).findFirst().get();
+
+//          SOLUTION 2:
+//        for (User user : users) {
+//            if(user.getId().equals(id)) {
+//                return user;
+//            }
+//        }
+//        // Return null if no user with the specified ID is found
+//        return null;
     }
 }
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,33 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private final UserDaoService service;
+
+
+    public UserResource(UserDaoService service){
+        this.service = service;
+    }
+
+
+    // GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+       return service.findById(id);
+    }
+
+
+}
